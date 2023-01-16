19f8f83a738403fe3815714d40c7d8f2c53ab714
ABHISHEK VERMA <104015898+AbhiiiDev@users.noreply.github.com>
2023-01-17 01:12:20 +0530

    editor section added

--- src/components/Editor/Editor .jsx
+++ src/components/Editor/Editor .jsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import styles from './Editor.module.css'
+import { useState } from 'react';
 
 function Editor (props) {
     const sections=props.sections;
+const [ActiveSectionKey, setActiveSectionKey] = useState(
+  Object.keys(sections)[0]
+)
+
   return (
     <div className={styles.container}>
       <div className={styles.header}>
         {Object.keys(sections)?.map((key)=>(
-            <div className={styles.sections} key={key}>
+            <div className={`${styles.section} ${
+              ActiveSectionKey===key ? styles.active : ""
+              }`
+            }
+            key={key}
+            onClick={()=>setActiveSectionKey(key)}>
                 {sections[key]}
+              
                 </div>
         ))}
       </div>
--- src/components/Header/body/Body.jsx
+++ src/components/Header/body/Body.jsx
@@ -1,40 +1,41 @@
 import React from 'react'
 import Editor from '../../Editor/Editor '
 import styles from './Body.module.css'
+import {ArrowDown} from 'react-feather'
 
 function Body() {
-    const colors=[ "#239ce2" , "#48bb78" , "#0bc5ea", "#a0aec0" , "#ed8936"  ];
-    const sections ={
-        basinInfo:"Basic Info",
-        workExp:"Work Experience",
-        project:"Project",
-        education:"Education",
-        achievements:"Achievements",
-        summary:"Summary",
-        others:"Others"
-    }; 
-  return (
-    <div className={styles.container}>
-<p className={styles.heading}>Resume Builder</p>
-<div className={styles.toolbar}>
-    <div className={styles.colors}>
-{
-    colors.map((item)=>(
-        <span className={styles.color} 
-        key={item}
-style={{backgroundColor:item}}
+    const colors = ["#239ce2", "#48bb78", "#0bc5ea", "#a0aec0", "#ed8936"];
+    const sections = {
+        basinInfo: "Basic Info",
+        workExp: "Work Experience",
+        project: "Project",
+        education: "Education",
+        achievements: "Achievements",
+        summary: "Summary",
+        others: "Others"
+    };
+    return (
+        <div className={styles.container}>
+            <p className={styles.heading}>Resume Builder</p>
+            <div className={styles.toolbar}>
+                <div className={styles.colors}>
+                    {
+                        colors.map((item) => (
+                            <span className={styles.color}
+                                key={item}
+                                style={{ backgroundColor: item }}
 
-/>
-        ))
-}
-    </div>
-    <button>download</button>
-</div> 
-<div className={styles.main}>
-    <Editor sections={sections}/>
-</div>
-    </div>
-  ) 
+                            />
+                        ))
+                    }
+                </div>
+                <button>download <ArrowDown className='svg'/>    </button>
+            </div>
+            <div className={styles.main}>
+                <Editor sections={sections} />
+            </div>
+        </div>
+    )
 }
 
 export default Body
